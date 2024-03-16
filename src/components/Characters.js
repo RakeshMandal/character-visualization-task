7fbc22ccb8650251db3aacb91224e48c4bb7fd3a
Rakesh Kumar Mandal <rakeshmandal0502@gmail.com>
2024-03-17 01:11:46 +0530

    added previous, next button and applyed some css for look nice

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -7,11 +7,12 @@ const Characters = () => {
 
     const { setSelectedCharacterId } = useContext(CharacterContext);
     const [characters,setCharecters] = useState([]);
+    const [page, setPage] = useState(1);
 
     useEffect(()=>{
        async function getCharacterfromAPI(){
             try{
-                const response = await fetch(API_URL)
+                const response = await fetch(`https://rickandmortyapi.com/api/character/?page=${page}`)
                 const data = await response.json()
                 setCharecters(data.results)
             }
@@ -21,21 +22,35 @@ const Characters = () => {
             }
         }
         getCharacterfromAPI()
-    },[]);
+    },[page]);
 
     const handleCharacterClick = (id) => {
         setSelectedCharacterId(id);
       };
 
+      const nextPage = () => {
+        setPage(prevPage => prevPage + 1);
+      };
+    
+      const prevPage = () => {
+        if (page > 1) {
+          setPage(prevPage => prevPage - 1);
+        }
+      };
+
 
   return (
     <>
-    <h1 style={{textAlign:"center"}}>Character Visualization Application</h1>
+    <h1 style={{textAlign:"center",color:"#222222"}}>Character Visualization Application</h1>
+    <div className='prev-next-btn'>
+    <button onClick={prevPage} className='btn'>Previous Page</button>
+    <button onClick={nextPage} className='btn nextbtn'>Next Page</button>
+    </div>
    
     <div className='showed-characters'>
       {
         characters.map((item,id)=>(
-            <div key={id}>
+            <div key={id} className='character-showing'>
             <Link to={`/character/${item.id}`} onClick={() => handleCharacterClick(item.id)}>
 
                 <img src={item.image} />
